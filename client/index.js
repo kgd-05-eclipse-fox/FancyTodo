bc62d137a22b927d1c82dd65ab1343e05ee43df6
abrarnaim015 <aabrarnaim24@gmail.com>
2020-10-30 11:36:49 +0700

    add singup proces

--- client/index.js
+++ client/index.js
@@ -5,9 +5,11 @@ $(document).ready(()=>{
     if(token){
         $('#content-page').show()
         $('#login-page').hide()
+        $('#singup-page').hide()
     }else{
         $('#login-page').show()
         $('#content-page').hide()
+        $('#singup-page').hide()
     }
 })
 
@@ -32,6 +34,7 @@ function userLogin(e){
         localStorage.setItem('token', token)
         $('#content-page').show()
         $('#login-page').hide()
+        $('#singup-page').hide()
     })
     .fail(err=>{
         console.log(err)
@@ -39,14 +42,64 @@ function userLogin(e){
     
 }
 
+function userSinUp(e){
+    e.preventDefault()
+    let email = $('#login-email').val()
+    let password = $('#login-password').val()
+    console.log(email, password)
+
+    $.ajax({
+        method: "POST",
+        url: SERVER + '/user/regis',
+        data: {
+            email,
+            password
+        }
+    })
+    .done(res=>{
+        console.log(res)
+        const token = res.token
+        localStorage.setItem('token', token)
+        $('#content-page').hide()
+        $('#singup-page').hide()
+        $('#login-page').show()
+    })
+    .fail(err=>{
+        console.log(err)
+    })
+}
+
 $('#btn-logout').on('click', ()=>{
     logout()
 })
+
+$('#user-singUp').on('click', ()=>{
+    singup()
+})
+
+$('cancel-singup').on('click', ()=>{
+    cancelSingup()
+})
+
+function cancelSingup(){
+    $('#content-page').hide()
+    $('#login-page').show()
+    $('#singup-page').hide()
+    localStorage.clear()
+}
+
+function singup(){
+    $('#content-page').hide()
+    $('#login-page').hide()
+    $('#singup-page').show()
+    localStorage.clear()
+}
 
 function logout(){
     // e.preventDefault()
     $('#content-page').hide()
     $('#login-page').show()
+    $('#singup-page').hide()
     localStorage.clear()
 }
 
