222f57c806b2023ce35583802cdffd240eec1cc8
abrarnaim015 <aabrarnaim24@gmail.com>
2020-10-30 11:09:12 +0700

    add login, home page, and logout with token

--- client/index.js
+++ client/index.js
@@ -1,3 +1,55 @@
+const SERVER = 'http://localhost:3000'
+
+$(document).ready(()=>{
+    const token = localStorage.getItem('token')
+    if(token){
+        $('#content-page').show()
+        $('#login-page').hide()
+    }else{
+        $('#login-page').show()
+        $('#content-page').hide()
+    }
+})
+
+
+function userLogin(e){
+    e.preventDefault()
+    let email = $('#login-email').val()
+    let password = $('#login-password').val()
+    console.log(email, password)
+
+    $.ajax({
+        method: "POST",
+        url: SERVER + '/user/login',
+        data: {
+            email,
+            password
+        }
+    })
+    .done(res=>{
+        console.log(res)
+        const token = res.token
+        localStorage.setItem('token', token)
+        $('#content-page').show()
+        $('#login-page').hide()
+    })
+    .fail(err=>{
+        console.log(err)
+    })
+    
+}
+
+$('#btn-logout').on('click', ()=>{
+    logout()
+})
+
+function logout(){
+    // e.preventDefault()
+    $('#content-page').hide()
+    $('#login-page').show()
+    localStorage.clear()
+}
+
 
 // $('.btn').on('click', ()=>{
 //     console.log('masukkkkkkkkkk')
@@ -16,11 +68,3 @@
 //     console.log(email)
 //     console.log(password)
 // })
-
-function userLogin(e){
-    e.preventDefault()
-    // let email = $('#login-email')
-    // let password = $('#login-password')
-    console.log('masuk=>>>>>>>>>>>>>>')
-    
-}
--- server/app.js
+++ server/app.js
@@ -1,11 +1,13 @@
 require('dotenv').config()
 
 const express = require('express')
+const cors = require('cors')
 const app = express()
 const port = process.env.PORT
 const routers = require('./routes')
 const errorHandler = require('./middlewares/errorHandler.js')
 
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.use(routers)
