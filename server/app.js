1b6b71b0ae76329ed49936bd1c3d15131cb9845a
jeplindm <jeplindm@github.com>
2020-11-02 22:41:42 +0700

    fix port and package.json

--- server/app.js
+++ server/app.js
@@ -1,9 +1,12 @@
-require('dotenv').config();
+
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+}
 
 const express = require('express');
 const app = express();
-const port = process.env.PORT;
 const cors = require('cors');
+const port = process.env.PORT || 3000;
 const routes = require('./routes/index');
 const errorHandler = require('./middlewares/error-handler');
 
